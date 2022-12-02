1374a58f4ed2dcf97fec68de4dcb8545df8c9bec
Loic Denuziere <loic@denuziere.net>
2022-12-02 11:02:15 +0100

    Day 1

---
+++ Aoc2022/Common.fs
@@ -0,0 +1,13 @@
+﻿[<AutoOpen>]
+module Common
+
+open System.IO
+
+let load (value: string) =
+    value.Split('\n')
+    |> List.ofArray
+
+let loadInput name =
+    let path = Path.Combine(__SOURCE_DIRECTORY__, "inputs", name + ".txt")
+    File.ReadAllLines path
+    |> List.ofArray
